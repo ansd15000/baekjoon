d9d98087c285398db6590282ffa75f490c0ce508
ansd15000 <ansd1500@gmail.com>
2021-06-30 02:40:23 +0900

    .

--- step/level 10 (재귀)/2447_별찍기-10.py
+++ step/level 10 (재귀)/2447_별찍기-10.py
@@ -1,34 +1,57 @@
-a = int(input()) #전체 사각형 크기 
-b = int(a / 3) # 사각형 가로세로길이. 가운데 공백 있어야함
-c = ['***', '* *', '***']
-d = ['   ']
+# 너무어려워
+# N = int(input()) # 전체 사각형 크기. 항상 3의 거듭제곱
+# innerSquare = int(N / 3) # # 내부 사각형의 크기
 # c = ['***', '* *', '***']
-# res = None
-# for i in range(b):
-#     res+=c
+# blank = '   '
+# # 딱 9 * 9 까지 가능
+# def square(a):
+#     interval = int(a / 3) if a >=9 else 1# 가운데 공백 
+#     result = ''
+#     print(f'interval: {interval}')
+#     for i in range(a): # 한줄씩 출력합니다
+#         if interval <= i < a - interval: # 중간 공백을 출력하는 부분.. /3 해도 될듯?
+#             result += '\n'.join(map(lambda x : (x * interval) + (blank * interval) + (x * interval), c)) + '\n'
+#         else : # 중간공백을 제외한 출력. 단, 패턴으로 출력되어야함.
+#             # result += '\n'.join(map(lambda x : x * a, c)) + '\n' # 27 이상으로 하면 각 9 x 9  위치에 빈공간을 생성하는법을 알아야함      
+#             result += '\n'.join(map(lambda x : x * a, c)) + '\n'
+#             pass
+    
+#     return result.rstrip()
 
-def n_start(size):
-    result = ''
-    for i in range(int(size / 3)): # 만약 9면 3으로
-        if int(size / 2) == i : # 비어있어야 할 중간부분일 경우
-            result += d + '\n'
-            print(d + '\n')
-        else:
-            print((c * size) + '\n') 
-            result += (c * size) + '\n'
-    return result
+# def main():
+#     innerSquare = int(N / 3)
+#     result = ''
+#     if innerSquare < 9 :
+#         print(square(innerSquare))
+#     else:
+#         a = square(3) # 큰수의 최소 (가로세로 9)
+#         interval = int(innerSquare / 3) # N이 27 기준 innersquare는 9, 이걸 3줄동안 출력
+#         for _ in range(interval):
+#             # if i == interval / 3:
+#             #     pass
+#             # else: 
+#             #     print('\n'.join(map(lambda x: x*interval, a.split('\n'))))
+#             result += '\n'.join(map(lambda x: x*interval, a.split('\n'))) + '\n'
+#         result = result.rstrip().split('\n')
+#         for i in range(innerSquare):
+#             replacing = result[innerSquare + i] # 별이 있는 문자열
+#             data = replacing[innerSquare: -innerSquare].replace('*',' ')
+#             newdata = replacing[:innerSquare] + data + replacing[-innerSquare:]
+#             result[innerSquare + i] = newdata
+#         print('\n'.join(result))
+#         return result
+# main()
 
-print(n_start(b))
+def concatenate(r1, r2):
+    return [''.join(x) for x in zip(r1, r2, r1)] # *** 맨위 아래
 
-# 1
-# 1-1 싸이클은 걍 곱셈
-# 1-2 싸이클은 b / 3 / 2 위치에 빈값 넣고
-# 1-3 싸이클은 처음과 동일
+def star10(n):
+    if n == 1:
+        return ['*']
+    n //= 3
+    x = star10(n)
+    top_bottom = concatenate(x, x)
+    middle = concatenate(x, [' '*n]*n)
+    return top_bottom + middle + top_bottom
 
-# 2
-# 2-1 싸이클은 0~9, 9~19, 18 ~ 28 순서대로 곱, 공백 곱
-# 2-2 싸이클은
-# 2-3 싸이클은
-
-# 3
-# 1 사이클과 동일
+print('\n'.join(star10(int(input()))))
--- step/level 9 (default math 2)/1929_소수구하기.py
+++ step/level 9 (default math 2)/1929_소수구하기.py
@@ -1,5 +1,41 @@
-import sys
-a, b = map(int, sys.stdin.readline().rstrip().split())
+# a, b = map(int, input().split())
+# for i in range(a, b + 1):
+#     sosu = True
+#     for j in range(2, i):
+#         if i % j == 0 : 
+#             sosu = False
+#             break
+#     if sosu: 
+#         print(i)
 
-for i in range(a, b + 1):
-    print(i)
+# 배꼇어요...
+def z(num):
+    if num == 1:
+        return False
+    for i in range(2, int(num**.5) + 1):
+        if num % i == 0: return False
+    return True
+
+a, b = map(int, input().split())
+
+for i in range(a, b+ 1 ):
+    if z(i):
+        print(i)
+
+
+# def prime_list(a, b):
+#     # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
+#     sieve = [True] * b
+ 
+#     # n의 최대 약수가 sqrt(b) 이하이므로 i=sqrt(b)까지 검사
+#     m = int(b ** 0.5)
+#     for i in range(a, m + 1):
+#         if sieve[i] == True:           # i가 소수인 경우 
+#             for j in range(i+i, b, i): # i이후 i의 배수들을 False 판정
+#                 sieve[j] = False
+ 
+#     # 소수 목록 산출
+#     return [i for i in range(a, b) if sieve[i] == True]
+
+# a, b = map(int, input().split())
+# print(prime_list(a, b))
