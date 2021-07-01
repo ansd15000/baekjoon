2834fb476f2e7777c118485245bc8d8d1b994891
ansd15000 <ansd1500@gmail.com>
2021-07-02 04:24:14 +0900

    .

--- step/level 16 (greedy)/1541_잃어버린 괄호.py
+++ step/level 16 (greedy)/1541_잃어버린 괄호.py
@@ -22,15 +22,20 @@
 
 # 숫자가 0으로 시작할 수 있다는게.. 01 09 이딴거였음
 # 너무 바보처럼 짠거같은데.. ㅋㅋ
-a = input().split('-')
-l = [list(map(int, i.split('+'))) for i in a]
-for i,v in enumerate(l):
-    if len(v) > 1 : l[i] = [str(sum(map(int, l[i])))]  # 0으로 시작되는 문자형 숫자 제거를 위해 int변환후 연산후 문자변환
-    else : l[i] = [str(sum(map(int, l[i])))]           # 마찬가지로 int -> str
-l = sum(l, []) # 2차원을 1차원으로
-print(eval('-'.join(l)))
+# a = input().split('-')
+# l = [list(map(int, i.split('+'))) for i in a]
+# for i,v in enumerate(l):
+#     if len(v) > 1 : l[i] = [str(sum(map(int, l[i])))]  # 0으로 시작되는 문자형 숫자 제거를 위해 int변환후 연산후 문자변환
+#     else : l[i] = [str(sum(map(int, l[i])))]           # 마찬가지로 int -> str
+# l = sum(l, []) # 2차원을 1차원으로
+# print(eval('-'.join(l)))
 
-
+# sum이 0이 붙은 문자형 숫자를 변환해주는구나
+# a = '55-50+40+01-08'.split('-')
+a = input().split('-')                       # - 기준으로 리스트
+l = [sum(map(int, i.split('+'))) for i in a] # 리스트에서 +가 있는 요소를 더하고
+print(l[0] - sum(l[1:]))                     # 가장 앞의 값과 그 뒤에값을 합한걸 뺌.
+# 왜? - 기준으로 빼야할 대상은 항상 맨 앞에 있기 때문에. 대박이네.
 
 # 55-50+40-10+30+100
 # 55-50+40-(10+30+100)
